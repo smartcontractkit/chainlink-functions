ebd1e6a80166b7d244713511e3533af4473f335c
vyzaldysanchez <vyzaldysanchez@gmail.com>
2023-12-05 12:15:44 -0400

    Adds placeholder file

---
+++ main.go
@@ -0,0 +1,9 @@
+package chainlink_functions
+
+import "fmt"
+
+// TODO: This is a placeholder file to make CI work and pass, remove this comment when the real
+// code comes in.
+func main() {
+	fmt.Println("Chainlink Functions")
+}
